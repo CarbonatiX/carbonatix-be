13bf7b357781efe6ec17349c601bad2f00b2e685
unknown <indah.sekar523@gmail.com>
2026-07-31 10:31:39 +0700

    feat: update database connection for FastAPI

--- server/__init__.py
+++ server/__init__.py
@@ -1,2 +1,2 @@
-from db import init_connection, get_db
-from auth import login, register, get_all_users, delete_user, verify_token
+from .database import get_database
+from .auth import login, register, get_all_users, delete_user, verify_token
