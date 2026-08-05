16a7b0f5ef458dcf4d6460558c6eb1d0e39ba72c
unknown <indah.sekar523@gmail.com>
2026-08-05 19:37:37 +0700

    refactor: cleaning code

---
+++ server/database.py
@@ -0,0 +1,21 @@
+import pymongo
+from pymongo.errors import ConnectionFailure
+from .config import settings
+
+
+_client = None
+
+
+def get_database():
+    global _client
+    if _client is None:
+        try:
+            _client = pymongo.MongoClient(settings.MONGODB_URI)
+            _client.admin.command("ping")
+        except ConnectionFailure:
+            return None
+    return _client[settings.MONGODB_DB_NAME]
+
+
+def get_db():
+    return get_database()
